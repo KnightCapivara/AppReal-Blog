15a5a3a8566ea2c7d92561e1dce14811f2045354
Danillo Acassio <55496158+KnightCapivara@users.noreply.github.com>
2022-02-16 23:41:25 -0400

    update

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ApplicationRecord
   extend FriendlyId
 
   validates :title, :author, :body, presence: true
-
+  
+  #Destroi o comentario.
   has_many :comments, dependent: :destroy
 
   friendly_id :title, use: :slugged
